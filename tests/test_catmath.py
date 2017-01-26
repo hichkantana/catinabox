a2d106ada2966dcae8fead4bbc638299c3c4bf62
Hicham Ichkantana <hicham.ichkatnana@hp.com>
2017-01-26 08:47:51 -0800

    complete test cases

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -6,7 +6,7 @@ def test__cat_years_to_hooman_years__middle_age__succeeds():
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert catmath.cat_years_to_hooman_years(0.2) == 1.0
+    assert catmath.cat_years_to_hooman_years(0.1) == 0.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
@@ -16,3 +16,5 @@ def test__cat_years_to_hooman_years__0__returns_0():
 # what is this test doing?
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+    assert catmath.is_cat_leap_year(2015) is False
+    assert catmath.is_cat_leap_year(2000) is True
